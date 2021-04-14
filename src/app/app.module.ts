c965f8753d8792bda67b019db941e603632520d8
MukulMairh <msoni7718@gmail.com>
2021-04-14 23:34:12 +0530

    Added new component folder structure for this project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipesListComponent } from './recipies/recipes-list/recipes-list.component';
+import { RecipesDetailsComponent } from './recipies/recipes-details/recipes-details.component';
+import { RecipeItemComponent } from './recipies/recipes-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    RecipesListComponent,
+    RecipesDetailsComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
